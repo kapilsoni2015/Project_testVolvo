d0f721adef04a607d1b61a65b3a8c0262d59620d
Soni <Kapil.Soni@emc.com>
2021-11-28 21:06:23 +0530

    Updated scripts in test folder

---
+++ tests/test_two.py
@@ -0,0 +1,39 @@
+import unittest
+from selenium import webdriver
+import time
+import os
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+class VolvoCarSearch(unittest.TestCase):
+
+    def setUp(self):
+        #caps = {'browserName': os.getenv('BROWSER', 'chrome')}
+        #self.driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',desired_capabilities=caps)
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+                desired_capabilities=DesiredCapabilities.CHROME)
+        
+    def test_open_volveweb(self):
+        driver = self.driver
+        driver.get("https://www.volvocars.com/intl/v/car-safety/a-million-more")
+        
+    def test_accepting_cookies(self):
+        driver = self.driver
+        driver.get("https://www.volvocars.com/intl/v/car-safety/a-million-more")
+        time.sleep(3)
+        driver.find_element_by_css_selector("button[title='Accept']").click()
+        time.sleep(2)
+                
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+
+
+
+
